dfa3119abffd02301eafd5bb424a2a47b75b86cb
ak101z2 <arpsku17@gmail.com>
2024-02-20 22:26:44 +0000

    had to protect database password

--- database.py
+++ database.py
@@ -3,8 +3,6 @@ from sqlalchemy import create_engine, text
 
 db_connection_string = os.environ['DB_CONNECT_STRING']
 
-# "mysql+pymysql://hti7ln9xc95kd2xry3m1:pscale_pw_PxunMmQM4WfgThZ4FgeHHOdGAYncyaqYg8KFXrS6xD0@aws.connect.psdb.cloud/concierge-application?charset=utf8mb4"
-
 engine = create_engine(db_connection_string, connect_args={
   "ssl": {
     "ssl_ca": "/etc/ssl/cert.pem"
